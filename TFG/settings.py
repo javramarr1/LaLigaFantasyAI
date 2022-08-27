3f4674b7f992797aa319b8800c6eb8e0519247db
javramarr1 <jramosarrondo@gmail.com>
2022-08-27 10:42:16 +0200

    Cambios para despliegue

--- TFG/settings.py
+++ TFG/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'TFG',
-        'USER': 'admin',
-        'PASSWORD': 'Contrasena22.',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT':'5432',
     }
