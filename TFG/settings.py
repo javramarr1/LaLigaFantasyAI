6bdf089e892adcfd69d6ee6330da2c5d0ba04d8b
javramarr1 <jramosarrondo@gmail.com>
2022-08-27 11:04:43 +0200

    Cambios para despliegue

--- TFG/settings.py
+++ TFG/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-v#r@h&w(5y=*=(ea6fc@_bzacyd4ukhgoiz#itdui)%ivfbhqj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fantasytips.herokuapp.com']
 
 
 # Application definition
