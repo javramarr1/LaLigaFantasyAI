6717fd785bb5fd49deb4306dc7acb19f15d1022d
javramarr1 <jramosarrondo@gmail.com>
2022-08-28 09:57:58 +0200

    Cambios para despliegue

--- TFG/settings.py
+++ TFG/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'fantasy/assets/'
+STATIC_URL = '/fantasy/assets/'
 STATIC_ROOT = os.path.join(BASE_DIR, '/fantasy/assets')
 STATICFILES_FINDERS = (
 'django.contrib.staticfiles.finders.AppDirectoriesFinder',
