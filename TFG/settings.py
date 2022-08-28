1a5ebd3700ee5c7ad4cbcd8b05701a8ec00b8af1
javramarr1 <jramosarrondo@gmail.com>
2022-08-28 11:01:54 +0200

    Cambios para despliegue

--- TFG/settings.py
+++ TFG/settings.py
@@ -133,7 +133,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/assets/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/app/staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, '../assets')
+
+STATICFILES_FINDERS = (
+'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+'django.contrib.staticfiles.finders.FileSystemFinder',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
